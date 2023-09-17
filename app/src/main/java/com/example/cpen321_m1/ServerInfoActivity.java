31a2ac147286726c048105718b93081fbc8295ed
Justin Ng <ngjustin2002@gmail.com>
2023-09-17 00:07:58 -0700

    HTTP GET requests work with azure backend

--- app/src/main/java/com/example/cpen321_m1/ServerInfoActivity.java
+++ app/src/main/java/com/example/cpen321_m1/ServerInfoActivity.java
@@ -9,6 +9,16 @@ import android.text.format.Formatter;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
@@ -29,14 +39,36 @@ public class ServerInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server_info);
-
-        updateStringValues();
+        requestBackendAPIs();
+        updateClientValues();
         updateText();
     }
 
-    private void updateStringValues() {
-        WifiManager wifiMgr = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-        clientIPString = Formatter.formatIpAddress(wifiMgr.getConnectionInfo().getIpAddress());
+    private void updateClientValues() {
+//        WifiManager wifiMgr = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+//        clientIPString = Formatter.formatIpAddress(wifiMgr.getConnectionInfo().getIpAddress());
+        RequestQueue volleyQueue = Volley.newRequestQueue(this);
+        String ipifyUrl = "https://api.ipify.org?format=json";
+        StringRequest requestIpify = new StringRequest(Request.Method.GET, ipifyUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(TAG, "Response: " + response);
+                        try {
+                            JSONObject jObject = new JSONObject(response);
+                            clientIPString = jObject.getString("ip");
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                        updateText();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, String.valueOf(error));
+            }
+        });
+        volleyQueue.add(requestIpify);
 
         OffsetTime offset = null;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -50,6 +82,7 @@ public class ServerInfoActivity extends AppCompatActivity {
         assert b != null;
         loggedInString = b.getString("username");
     }
+
     private void updateText() {
         TextView serverIPText = (TextView) findViewById(R.id.server_ip_text);
         serverIPText.setText(String.format("Server IP address: %s", serverIPString));
@@ -69,4 +102,61 @@ public class ServerInfoActivity extends AppCompatActivity {
         TextView loggedInText = (TextView) findViewById(R.id.logged_in_text);
         loggedInText.setText(String.format("Logged in: %s", loggedInString));
     }
+
+    private void requestBackendAPIs() {
+        RequestQueue volleyQueue = Volley.newRequestQueue(this);
+        String urlIP = "http://20.239.76.13:8081/ip";
+        String urlTime = "http://20.239.76.13:8081/time";
+        String urlName = "http://20.239.76.13:8081/name";
+
+        StringRequest requestIP = new StringRequest(Request.Method.GET, urlIP,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(TAG, "Response: " + response);
+                        serverIPString = response;
+                        updateText();
+                    }
+                }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e(TAG, String.valueOf(error));
+                }
+        });
+
+        StringRequest requestTime = new StringRequest(Request.Method.GET, urlTime,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(TAG, "Response: " + response);
+                        serverTimeString = response;
+                        updateText();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, String.valueOf(error));
+            }
+        });
+
+        StringRequest requestMyName = new StringRequest(Request.Method.GET, urlName,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(TAG, "Response: " + response);
+                        myNameString = response;
+                        updateText();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, String.valueOf(error));
+            }
+        });
+        volleyQueue.add(requestIP);
+        volleyQueue.add(requestTime);
+        volleyQueue.add(requestMyName);
+    }
+
+
 }
